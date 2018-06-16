ce662e4f1f3c3b97f70afd7d6d2aaf0951e38572
jakub.skalak@gmail.com <jakub.skalak@gmail.com>
2018-06-16 21:24:33 +0200

    CC v5

--- Client/Client.java
+++ Client/Client.java
@@ -1,33 +1,18 @@
 package Client;
 
-public class Client
-{
-    public static void main( String args[] ) throws Exception
-    {
-        System.out.println("Client started, args count = " + args.length);
-
-        if (args.length < 3)
-        {
-            System.out.println("Usage: <case> <host> <serverPorts>...");
-            return;
-        }
-
-        String testcase = args[0];
-        String host = args[1];
+public class Client {
+    public static void main(String args[]) throws Exception {
+        String example = args[0];
+        String hostIP = args[1];
         String[] serversPorts = new String[args.length - 2];
 
         for(int i = 2;  i < args.length; ++i) {
             serversPorts[i - 2] = args[i];
         }
 
-
-        System.out.println("Getting map");
-        Graph graph = Graph.mapGraphFromFile("testcases/" + testcase);
+        Graph graph = Graph.mapGraphFromFile("cases/" + example);
         graph.printWeights();
 
-        System.out.println("Launching Dijkstra");
-        new DijkstraClient(graph, host, serversPorts).run();
-
-
+        new DijkstraClient(graph, hostIP, serversPorts).run();
     }
 }
